abf635d04aefc6c25d04174bcba93ec0433e47db
Josué Ortiz <168014672+NW08@users.noreply.github.com>
2025-07-24 17:45:04 -0500

    Login Screen FXML & CSS fixed

--- src/main/java/controllers/LoginScreenController.java
+++ src/main/java/controllers/LoginScreenController.java
@@ -1,4 +1,30 @@
 package main.java.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginScreenController {
+
+   @FXML
+   private Label lbl_message_login;
+
+   @FXML
+   private PasswordField pass_field;
+
+   @FXML
+   private TextField txt_field_email;
+
+   @FXML
+   void createAccountAction(ActionEvent event) {
+
+   }
+
+   @FXML
+   void loginAction(ActionEvent event) {
+
+   }
+
 }
